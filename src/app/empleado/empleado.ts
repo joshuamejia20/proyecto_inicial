7ea2b845967f275a7737176eefe28eb39743b69c
Josue Mejia <josueomejiap@gmail.com>
2025-08-27 19:25:41 -0600

    clase 270825

--- src/app/empleado/empleado.ts
+++ src/app/empleado/empleado.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-empleado',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './empleado.html',
   styleUrl: './empleado.css'
 })
@@ -10,10 +11,11 @@ export class Empleado {
   nombre = "Juan";
   apellido = "Pérez";
   edad = 15;
-  empresa = "UCAD";
+  empresa = "NEVERIA";
   habilitar_input_property = false;
   usuario_registrado_property = false;
   texto_registro = "No hay nadie registrado";
+  dato = "";
 
   constructor(){}
 
@@ -27,11 +29,18 @@ export class Empleado {
 
   cambiar_registro(){
     this.usuario_registrado_property = !this.usuario_registrado_property;
-    this.set_usuario_registrado();
+    //this.set_usuario_registrado();
   }
 
-  set_usuario_registrado(){
-    if(!this.usuario_registrado_property){
+  set_usuario_registrado(event: Event){
+    //alert(event.target);
+    //console.log(event);
+    if((<HTMLInputElement>event.target).value=="si"){
+      this.texto_registro = "El usuario se acaba de registrar";
+    }else{
+      this.texto_registro = "No hay nadie registrado";
+    }
+    /*if(!this.usuario_registrado_property){
       //alert(this.usuario_registrado_property);
       //alert("El usuario salió de la sesión")
       this.texto_registro = "No hay nadie registrado";
@@ -39,6 +48,18 @@ export class Empleado {
       //alert("El usuario  se ha registrado");
       //alert(this.usuario_registrado_property);
       this.texto_registro = "Usuario en línea";
-    }
+    }*/
+  }
+
+  presionar(){
+    console.log("Boton presionado");
+  }
+
+  cambiar_desde_input(event: Event){
+    this.dato = (event.target as HTMLInputElement).value;
+  }
+
+  cambiar_empresa(event: Event){
+    this.empresa = (<HTMLInputElement>event.target).value;
   }
 }
